bd795b54838aa100c2a7e25be9d8593b77acc5db
João Pedro Guttierrez <jpgutti08@hotmail.com>
2019-05-29 18:33:27 -0300

    Started Server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get("/teste", (req, res) => {
+    res.send("Ok, it worked")
+})
+
+app.listen(8080, () => console.log("Up and running"))
